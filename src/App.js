be508b277481dce3f9333ef73f74ed38b3e6e0a2
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-10-05 17:32:45 -0400

    initial boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Link 
+} from 'react-router-dom';
+
+
+const Contact = (props) => {
+  return(
+    <h2>Contact</h2>
+  )
+}
+
+const About = () => {
+  return(
+    <h2>About me</h2>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <h1>My Personal Webpage!</h1>
+          <Route path="/About" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
     );
   }
   
